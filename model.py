db221a7ff4d10434ea5e29507a148e8da7118f2f
Shane <shanethomaswilliams@gmail.com>
2024-03-17 20:36:22 -0230

    Final model

--- model.py
+++ model.py
@@ -1,63 +1,117 @@
 import os
+import time
 
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision.io import read_image
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
 
 import cv2 as cv
 import numpy as np
 
-from config import IMG_SIZE, LEARNING_RATE, EPOCHS, BATCH_SIZE
+from config import LEARNING_RATE, EPOCHS, BATCH_SIZE, TRAIN_RATIO
 
 class LandscapeDataset(Dataset):
-    def __init__(self, image_dir):
-        self.image_dir = image_dir
-        self.image_files = os.listdir(image_dir)
+    def __init__(self, image_dir, device, train=True):
+        self.device = device
+        all_files = [os.path.join(image_dir, file) for file in os.listdir(image_dir)]
+        num_files = len(all_files)
+        cutoff = int(num_files * TRAIN_RATIO)
+        self.image_files = all_files[:cutoff] if train else all_files[cutoff:]
+        print(f"{'Train:' if train else 'Test: '} Found {len(self.image_files)} images at {image_dir}")
 
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, idx):
-        color = cv.imread(os.path.join(self.image_dir, self.image_files[idx]))
+        color = cv.imread(self.image_files[idx])
         lab = cv.cvtColor(color, cv.COLOR_BGR2LAB)
         ab_trans = np.transpose(lab, (2, 0, 1))[1:]
         ab_tensor = torch.tensor(ab_trans).float()
-        ab_tensor_flat = torch.flatten(ab_tensor)
 
         grey = cv.cvtColor(color, cv.COLOR_BGR2GRAY)
         grey_exp = np.expand_dims(grey, axis=0)
         grey_tensor = torch.tensor(grey_exp).float()
 
-        return grey_tensor, ab_tensor_flat
+        return grey_tensor.to(self.device), ab_tensor.to(self.device)
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        img_pixels = IMG_SIZE * IMG_SIZE
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(img_pixels, int(img_pixels / 32)),
-            nn.ReLU(),
-            nn.Linear(int(img_pixels / 32), int(img_pixels / 128)),
-            nn.ReLU(),
-            nn.Linear(int(img_pixels / 128), int(img_pixels / 1024)),
-            nn.ReLU(),
-            nn.Linear(int(img_pixels / 1024), int(img_pixels / 8)),
-            nn.ReLU(),
-            nn.Linear(int(img_pixels / 8), img_pixels * 2),
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(64),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(256),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
+
+            nn.Softmax(dim=1),
+            nn.Conv2d(256, 2, kernel_size=1, padding=0, dilation=1, stride=1, bias=False),
+            nn.Upsample(scale_factor=4, mode='bilinear'),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        x = (x - 128) / 128.
+        logits = self.layers(x)
+        logits = (logits * 128.) + 128
         return logits
 
 
-def train_loop(dataloader, model, loss_fn, optimizer):
+def train_loop(dataloader, model, loss_fn, optimizer, epoch, start_time):
     size = len(dataloader.dataset)
     # Set the model to training mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
@@ -72,9 +126,9 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 10 == 0:
-            loss, current = loss.item(), batch * BATCH_SIZE + len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        loss, current = loss.item(), batch * BATCH_SIZE + len(X)
+        elapsed_seconds = int(time.time() - start_time)
+        print(f"{elapsed_seconds:05}: e{epoch} loss={loss:>7.2f} \t[{current:>5d}/{size:>5d}]")
 
 
 def test_loop(dataloader, model, loss_fn):
@@ -96,23 +150,23 @@ def test_loop(dataloader, model, loss_fn):
 
 
 def create_model():
-    train_dataset = LandscapeDataset("images/train")
-    test_dataset = LandscapeDataset("images/test")
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
+
+    train_dataset = LandscapeDataset("lhq_256", device, train=True)
+    test_dataset = LandscapeDataset("lhq_256", device, train=False)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
     model = NeuralNetwork().to(device)
     print(model)
 
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
 
+    start_time = time.time()
     for t in range(EPOCHS):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(train_loader, model, loss_fn, optimizer)
+        train_loop(train_loader, model, loss_fn, optimizer, t+1, start_time)
         test_loop(test_loader, model, loss_fn)
 
     return model
--- run.py
+++ run.py
@@ -1,29 +1,133 @@
 import sys
+import os
 
 import torch
-from torch import nn
 import cv2 as cv
 import numpy as np
 
-from config import IMG_SIZE
 from model import NeuralNetwork
+from config import IMG_SIZE
 
-if __name__ == '__main__':
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load('model.pth'))
-    input_raw = cv.imread(sys.argv[1])
-    input_gray = cv.cvtColor(input_raw, cv.COLOR_BGR2GRAY)
+SATURATION_COEFF = 1.5
+NUM_IMAGES = 1000
+OUTPUT_DIR = "output"
+VIDEO_INPUT_DIR = "videos"
+NUM_IMAGES = 90000
+
+def pass_image(model, input_gray):
     input_exp1 = np.expand_dims(input_gray, axis=0)
     input_exp2 = np.expand_dims(input_exp1, axis=0)
-    input_tensor = torch.tensor(input_exp2).float()
-
+    input_tensor = torch.tensor(input_exp2).to("cuda").float()
     logits = model(input_tensor)
 
-    output_raw = torch.unflatten(logits, 1, (2, IMG_SIZE, IMG_SIZE))
-    output_np = output_raw.detach().numpy()[0]
+    output_np = logits.detach().to("cpu").numpy()[0]
     output_cat = np.concatenate((input_exp1, output_np), axis=0)
     output_trans = output_cat.transpose(1, 2, 0)
     output_int = output_trans.astype(np.uint8)
-    output_lab = cv.cvtColor(output_int, cv.COLOR_LAB2BGR)
+    output_bgr = cv.cvtColor(output_int, cv.COLOR_LAB2BGR)
 
-    print(cv.imwrite("output.jpg", output_lab))
+    hsv = cv.cvtColor(output_bgr, cv.COLOR_BGR2HSV).astype(np.float32)
+    (h, s, v) = cv.split(hsv)
+    s = s * SATURATION_COEFF
+    s = np.clip(s, 0, 255)
+    hsv_sat = cv.merge([h,s,v])
+    output_bgr_sat = cv.cvtColor(hsv_sat.astype(np.uint8), cv.COLOR_HSV2BGR)
+
+    return output_bgr, output_bgr_sat
+
+def main(action, model_name):
+    output_dir = os.path.join(OUTPUT_DIR, model_name)
+    image_dir = os.path.join(output_dir, "images")
+    video_dir = os.path.join(output_dir, "videos")
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(image_dir, exist_ok=True)
+    os.makedirs(video_dir, exist_ok=True)
+
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load(f"{model_name}.pth"))
+    model.to("cuda")
+
+    if action == "image":
+        image = 0
+        while True:
+            if image % 100 == 0:
+                print(f"Processing image {image}")
+            image_path = f"lhq_256/{image:07}.png"
+            input_raw = cv.imread(image_path)
+            input_gray = cv.cvtColor(input_raw, cv.COLOR_BGR2GRAY)
+            output_bgr, output_bgr_sat = pass_image(model, input_gray)
+            input_gray = cv.cvtColor(input_gray, cv.COLOR_GRAY2BGR)
+
+            muxed = np.zeros((IMG_SIZE*2, IMG_SIZE*2, 3), dtype=np.uint8)
+            muxed[:IMG_SIZE, :IMG_SIZE] = input_raw
+            muxed[:IMG_SIZE, IMG_SIZE:] = input_gray
+            muxed[IMG_SIZE:, :IMG_SIZE] = output_bgr
+            muxed[IMG_SIZE:, IMG_SIZE:] = output_bgr_sat
+
+            cv.imshow("Image Result", muxed)
+            key = cv.waitKey(0) & 0xFF
+
+            if key == ord('q'):
+                break
+            elif key == 81:
+                image -= 1 if image > 0 else 0
+            elif key == 83:
+                image += 1 if image < NUM_IMAGES else 0
+            elif key == ord('s'):
+                output_path = os.path.join(image_dir, f"{image:07}.png")
+                cv.imwrite(output_path, muxed)
+                print(f"Saved image {image_path} to {output_path}")
+
+    elif action == "video":
+        video = "mountains"
+        input_video_path = os.path.join(VIDEO_INPUT_DIR, f"{video}.mp4")
+        cap = cv.VideoCapture(input_video_path)
+        fourcc = cv.VideoWriter_fourcc(*'MP4V')
+        output_video_path = os.path.join(video_dir, f"{video}.mp4")
+        out = cv.VideoWriter(
+            output_video_path, fourcc, 20.0, (IMG_SIZE * 2, IMG_SIZE * 2))
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # if frame is read correctly ret is True
+            if not ret:
+                break
+
+            size = min(*frame.shape[:2])
+            y_start = (frame.shape[0] - size) // 2
+            x_start = (frame.shape[1] - size) // 2
+            crop = frame[y_start:y_start+size, x_start:x_start+size]
+
+            color256 = cv.resize(crop, (IMG_SIZE, IMG_SIZE))
+            gray256 = cv.cvtColor(color256, cv.COLOR_BGR2GRAY)
+            colorized, colorized_sat = pass_image(model, gray256)
+            gray256 = cv.cvtColor(gray256, cv.COLOR_GRAY2BGR)
+
+            muxed = np.zeros((IMG_SIZE*2, IMG_SIZE*2, 3), dtype=np.uint8)
+            muxed[:IMG_SIZE, :IMG_SIZE] = color256
+            muxed[:IMG_SIZE, IMG_SIZE:] = gray256
+            muxed[IMG_SIZE:, :IMG_SIZE] = colorized
+            muxed[IMG_SIZE:, IMG_SIZE:] = colorized_sat
+
+            out.write(muxed)
+
+            cv.imshow("Video Result", muxed)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        out.release()
+
+    else:
+        print("Invalid action")
+
+if __name__ == '__main__':
+    try:
+        action = sys.argv[1]
+        model_name = sys.argv[2]
+    except IndexError:
+        print("Usage: python run.py <action> <model_name>")
+        sys.exit(1)
+
+    main(action, model_name)
